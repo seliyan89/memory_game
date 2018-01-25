6fc42f32ebd8e4f1de2097d678f353c3b195b671
Seliyan Silvarajoo <seliyansilvarajoo@Seliyans-MacBook-Pro.local>
2018-01-25 23:25:02 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var card =["queen","queen","king","king"];
+var cardsInPlay=[];
+var cardOne=card[0];
+cardsInPlay.push(cardOne);
+var cardTwo=card[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length===2) {
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match");
+	}else {
+		alert("Sorry, try again");
+	}
+}
