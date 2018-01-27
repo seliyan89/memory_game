dd4d7b085951dfdebb05c6505d5794ab94277dd0
Seliyan Silvarajoo <seliyansilvarajoo@Seliyans-MacBook-Pro.local>
2018-01-27 20:16:12 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-var cards =["queen","queen","king","king"];
+var cards=[
+	{rank:"queen",
+	 suit:"hearts",
+	 cardImage:"images/queen-of-hearts.png"},
+	{rank:"queen",
+	 suit:"diamonds",
+	 cardImage:"images/queen-of-diamonds.png"},
+	{rank:"king",
+	 suit:"hearts",
+	 cardImage:"images/king-of-hearts.png"},
+ 	{rank:"king",
+	 suit:"diamonds",
+	 cardImage:"images/king-of-diamonds.png"}
+];
 var cardsInPlay=[];
 var checkForMatch=function(){
 	if (cardsInPlay.length===1) {
-	return 
+	return; 
 	} else {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -10,10 +23,12 @@ var checkForMatch=function(){
 		alert("Sorry, try again.");
 		}
 	}	
-}
+};
 var flipCard=function(cardID) {
-	console.log("User flipped "+cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped "+cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch();
 }
 flipCard(0);
