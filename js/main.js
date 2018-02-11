c474090573635ac11e112435d73623de00918a81
Seliyan Silvarajoo <seliyansilvarajoo@Seliyans-MacBook-Pro.local>
2018-02-11 21:10:44 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,47 +1,131 @@
-var cards=[
+var cardList=[
 	{rank:"queen",
 	 suit:"hearts",
-	 cardImage:"images/queen-of-hearts.png"},
+	 cardImage:"images/queen-of-hearts.png",
+	 selected:false,
+	 id:null
+	},
 	{rank:"queen",
 	 suit:"diamonds",
-	 cardImage:"images/queen-of-diamonds.png"},
+	 cardImage:"images/queen-of-diamonds.png",
+	 selected:false,
+	 id:null
+	},
 	{rank:"king",
 	 suit:"hearts",
-	 cardImage:"images/king-of-hearts.png"},
+	 cardImage:"images/king-of-hearts.png",
+	 selected:false,
+	 id:null
+	},
  	{rank:"king",
 	 suit:"diamonds",
-	 cardImage:"images/king-of-diamonds.png"}
+	 cardImage:"images/king-of-diamonds.png",
+	 selected:false,
+	 id:null
+	}
 ];
-var cardsInPlay=[];
-var checkForMatch=function(){
-	cardElement.setAttribute('src',cards[cardID].cardImage)
-	if (cardsInPlay.length===1) {
-	return; 
-	} else {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-		} else {
-		alert("Sorry, try again.");
-		}
-	}	
-};
 
-var flipCard=function() {
-	var cardID = this.getAttribute("data-id");
-	cardsInPlay.push(cards[cardID].rank);
-	checkForMatch();
-};
+var card = [];
+var counter=0;
+var successCount=0;
+var attemptCount=1;
 
-var createBoard=function(){
-	for(i=0;i<cards.length;i++){
-		var cardElement=document.createElement('img');
-		cardElement.setAttribute('src','images/back.png');
-		cardElement.setAttribute('data-id',i);
-		cardElement.addEventListener("click",function(){
-			flipCard();
-		});
-		document.getElementById('game-board').appendChild(cardElement);
+var createBoard =function(){ 
+	document.getElementById("scoreUnderline").innerHTML=successCount;
+	document.getElementById("attemptUnderline").innerHTML=attemptCount;
+	for (i=0;i<cardList.length;i++){
+		card.push(document.createElement('img'));
+		card[i].setAttribute('src','images/back.png');
+		card[i].setAttribute('data-selected',false);
+		card[i].setAttribute('data-ID',i);
+		document.getElementById('game-board').appendChild(card[i]);
+		flipCard(i);
 	};
 };
 
+var checkToReset = function(){
+	counter=0;
+	for (z=0;z<card.length;z++){
+		if (card[z].getAttribute('data-selected')==="true"){
+			counter+=1;
+		};
+	};	
+};
+
+var flipCard = function(i){
+		card[i].addEventListener('click',function(){
+			checkToReset();
+			if (counter===2){
+				return;
+			}else{
+				if (this.getAttribute('data-selected')==="true"){
+					alert("Please select another card!");
+				}else {
+					var randomnumber = Math.floor(Math.random()*4);
+					this.setAttribute('src',cardList[randomnumber].cardImage);
+					this.setAttribute('data-selected',"true");
+					this.setAttribute('data-rank',cardList[randomnumber].rank);
+					var currentCard=+(this.getAttribute('data-ID'));
+					checkForMatch(currentCard);
+				};	
+			};
+		});
+};
+
+var checkForMatch = function(currentCard){
+	for (a=0;a<card.length;a++){
+		if (a===currentCard || card[a].getAttribute('data-selected')==="false"){
+			continue
+		}else{
+			if (card[a].getAttribute('data-rank')===card[currentCard].getAttribute('data-rank')){
+				alert("Congratulations! You won!");
+				updateScore();
+				resetButtonFn();
+			}else{
+				alert("Sorry, not a match:( Try Again!");
+				resetButtonFn();
+			};
+		};
+	};
+};
+
+var resetButtonFn = function(){
+
+	var resetButtonNode=document.getElementById('resetButton');
+		while (resetButtonNode.hasChildNodes()) {
+			    resetButtonNode.removeChild(resetButtonNode.lastChild);
+		};
+	var resetButton=document.createElement('button');
+	resetButton.className += 'resetButtonClass';
+	resetButton.innerHTML="Click for next round";
+	resetButtonNode.appendChild(resetButton);
+	resetButton.addEventListener('click',function(){
+		//remove all cards in gameboard
+		var gameboard = document.getElementById('game-board');
+		while (gameboard.hasChildNodes()) {
+		    gameboard.removeChild(gameboard.lastChild);
+		};
+		card=[];
+		updateAttempt();
+		//recreate board
+		createBoard();
+		var resetButtonNode=document.getElementById('resetButton');
+		while (resetButtonNode.hasChildNodes()) {
+			    resetButtonNode.removeChild(resetButtonNode.lastChild);
+		};
+	});
+};
+
+var updateScore = function(){
+	successCount+=1;
+	document.getElementById("scoreUnderline").innerHTML=successCount;
+};
+
+var updateAttempt = function(){
+	attemptCount+=1;
+	document.getElementById("attemptUnderline").innerHTML=attemptCount;
+};
+
 createBoard();
+
+
